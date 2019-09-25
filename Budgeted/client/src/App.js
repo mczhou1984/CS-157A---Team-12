ed23a4d45e1840837f8565ddb752cadc18144e36
Tuong Nguyen <tuong.nguyen42@yahoo.com>
2019-09-24 22:41:45 -0700

    initial 3-tier connection

---
+++ Budgeted/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Users from './components/users/users';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Users/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Budgeted/client/src/components/users/users.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import './users.css';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+
+
+  useEffect(() => {
+    async function fetchMyAPI(){
+      const url = '/user/profile';
+      const response = await fetch(url);
+      const json =  await response.json();
+      setUsers(json);
+
+    }
+    fetchMyAPI();
+  }, []);
+
+
+
+  return (
+    <div className="users">
+    <h2>Users</h2>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+
+    </div>
+  );
+}
+
+
+export default Users;
