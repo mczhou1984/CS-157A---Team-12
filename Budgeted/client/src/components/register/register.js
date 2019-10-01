b022fb9975c02304a8b54e2677ee60b84bfd79b2
Tuong Nguyen <tuonguyen@blackberry.com>
2019-10-01 12:56:40 -0700

    added register react component and form validation

--- Budgeted/client/src/components/register/register.js
+++ Budgeted/client/src/components/register/register.js
@@ -1,13 +1,56 @@
 import React, {useState, useEffect} from 'react';
-import './register.css';
+import './register.module.scss';
+import useFormValidation from "../useFormValidation";
+import validateAuth from "../validateAuth";
+
+const INITIAL_STATE = {
+  email: "",
+  password: ""
+};
 
 function Register() {
-  const [users, setUsers] = useState([]);
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateAuth);
+  // const [email, setEmail] = React.useState("");
+  // const [password, setPassword] = React.useState("");
+
 
   return (
-    <div>
-    <h2>Register</h2>
-
+    <div className="container">
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="email"
+          value={values.email}
+          className={errors.email && "error-input"}
+          autoComplete="off"
+          placeholder="Your email address"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          className={errors.password && "error-input"}
+          name="password"
+          type="password"
+          placeholder="Choose a safe password"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+        <div>
+          <button class="btn btn-primary" disabled={isSubmitting} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
