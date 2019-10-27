f4723c98b36f99db2284de8f52a5927c4584c3a9
Tuong Nguyen <tuong.nguyen42@yahoo.com>
2019-10-27 00:23:12 -0700

    password hashed and stored in db when user is registered

--- Budgeted/server/routes.js
+++ Budgeted/server/routes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
+const config = require('./config/database');
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password:'password',
-  database: 'budgeteddb'
-});
+const db = mysql.createConnection(config);
 
 
 router.get('/profile', (req, res) => {
   let sql = 'SELECT accountId,name, email FROM accounts';
-  db.query(sql, (err, result) =>{
+  db.query(sql, (err, result) => {
     if (err) throw err;
     res.json(result);
   })
@@ -22,14 +20,15 @@ router.get('/profile', (req, res) => {
 
 router.post('/register', (req, res) => {
   const user = req.body;
-  let sql = 'INSERT into accounts (name, email, password) VALUES(?,?,?)';
-  db.query(sql, [user.name, user.email, user.password],(err, result) =>{
-    if(err){
-      res.json({success: false, msg:'Failed to register user'});
-    } else {
-      res.json({success: true, msg:'User registered'});
-}
-  })
+  User.addUser(user, (err) => {
+  if(err){
+    res.json({success: false, msg:'Failed to register user'});
+  } else {
+    res.json({success: true, msg:'User registered'});
+  }
+})
+
+
 
 
 })
