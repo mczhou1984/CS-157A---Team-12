6b0999b0a44cc21c968dde434f82ab9679cd7d56
Stephen Liu <stephenliu.official@gmail.com>
2019-11-10 21:12:48 -0800

    bugfix/add-percent-input-for-income

--- Budgeted/client/src/components/income/index.js
+++ Budgeted/client/src/components/income/index.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 export default function() {
     const [amount, setAmount] = useState('')
     const [type, setType] = useState('')
+    const [percent, setPercent] = useState('')
 
     return (
         <div style={{ marginTop: '20px' }}>
             <input value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Insert a type" />
             <input value={type} onChange={(e) => setType(e.target.value)} placeholder="Insert an amount" />
+            <input value={percent} onChange={(e) => setPercent(e.target.value)} placeholder="Insert a percent saving" />
         </div>
     )
 }
