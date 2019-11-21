2eef57706ebdd0dfc83f7812aa35df226f481f3c
Stephen Liu <stephenliu.official@gmail.com>
2019-11-20 22:18:47 -0800

    add input/expense/saving forms

--- Budgeted/client/src/components/budget/index.js
+++ Budgeted/client/src/components/budget/index.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
+import { StyledLogin } from '../login/login.styled.js';
 
 const FORM_TYPES = {
     INCOME: 'INCOME_FORM',
-    EXPENSES: 'EXPENSES_FORM'
+    EXPENSES: 'EXPENSES_FORM',
+    SAVINGS: 'SAVINGS_FORM',
 }
 
 const READABLE_FORM_NAMES = {
     [FORM_TYPES.INCOME]: 'Income',
     [FORM_TYPES.EXPENSES]: 'Expenses',
+    [FORM_TYPES.SAVINGS]: 'Savings',
 }
 
 export default function() {
     const [currentFormType, setFormType] = useState(null)
     const [amount, setAmount] = useState('')
     const [type, setType] = useState('')
-    const [percent, setPercent] = useState('')
+
+    function onBackClick() {
+        setFormType(null)
+        setAmount('')
+        setType('')
+    }
 
     if (!currentFormType) {
         return (
@@ -22,24 +30,27 @@ export default function() {
                 <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', maxWidth: 500, height: 100 }}>
                     <button onClick={() => setFormType(FORM_TYPES.INCOME)} >Income</button>
                     <button onClick={() => setFormType(FORM_TYPES.EXPENSES)} >Expenses</button>
+                    <button onClick={() => setFormType(FORM_TYPES.SAVINGS)} >Savings</button>
                 </div>
             </div>
         )
     }
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px'}}>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: 500, marginTop: 30, margin: '0 auto' }}>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <button style={{ width: 50, height: 25 }} onClick={() => setFormType(null)}>Back</button>
-                    <h1>{READABLE_FORM_NAMES[currentFormType]}</h1>
+        <StyledLogin>
+            <div style={{ maxWidth: 600, margin: 'auto', padding: 10, display: 'flex', flexDirection: 'column', marginTop: '20px'}}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: 500, marginTop: 30, margin: '0 auto' }}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <button className="btn btn-primary" style={{width: 200}} onClick={onBackClick}>Back</button>
+                        <h1>{READABLE_FORM_NAMES[currentFormType]}</h1>
+                    </div>
+                    {
+                        currentFormType !== FORM_TYPES.SAVINGS &&
+                        <input type="number" className="form-control mr-sm-2" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Insert a type" />
+                    }
+                    <input type="number" className="form-control mr-sm-2" value={type} onChange={(e) => setType(e.target.value)} placeholder="Insert an amount" />
+                    <button className="btn btn-primary" style={{width: 200}} onClick={onBackClick}>Submit</button>
                 </div>
-                <input value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Insert a type" />
-                <input value={type} onChange={(e) => setType(e.target.value)} placeholder="Insert an amount" />
-                {
-                    currentFormType === FORM_TYPES.INCOME &&
-                    <input value={percent} onChange={(e) => setPercent(e.target.value)} placeholder="Insert a percent saving" />
-                }
             </div>
-        </div>
+        </StyledLogin>
     )
 }
